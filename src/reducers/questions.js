f944beee4ddb3f21399ecb002b2c2db830e28509
Jennifer Gilbert <j.h.gilbert@gmail.com>
2019-06-14 16:44:29 -0700

    refactor: Give each question an ID

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,12 +1,19 @@
 import { ADD_QUESTION } from '../actions/questions'
 
-export default function questions (state = [], action) {
+// steal the ID functionality from _DATA.js for now,
+// so we can delay read/writes to the API until later
+function generateUID () {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+export default function questions (state = {}, action) {
   switch(action.type) {
     case ADD_QUESTION :
-      return [
+      const id = generateUID()
+      return {
         ...state,
-        action.question
-      ]
+        [id]: action.question
+      }
     default :
       return state
   }
