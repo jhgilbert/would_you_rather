3e1e0238c743b28d1bbd3bb09c6b036d9772f9ab
Jennifer Gilbert <j.h.gilbert@gmail.com>
2019-06-14 16:37:26 -0700

    feat: Add questions to state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { setAuthedUser } from '../actions/authedUser'
+import { addQuestion } from '../actions/questions'
 
 class App extends Component {
   render() {
     this.props.dispatch(setAuthedUser(1))
+    const question = {
+      options: [
+        { text: "Climb a tree", voters: [] },
+        { text: "Run a race", voters: [] }
+      ]
+    }
+    this.props.dispatch(addQuestion(question))
+
     return (
       <div className="App">
         This is the app component.
