8f0b18ddd49ce487fdc6423d8ce393230cfa21a4
Jennifer Gilbert <j.h.gilbert@gmail.com>
2019-06-14 18:03:42 -0700

    feat: Flesh out NewQuestion component UI

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,15 +2,60 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class NewQuestion extends Component {
+  state = {
+    optionOne: '',
+    optionTwo: ''
+  }
+
+  handleOptionOneChange = (e) => {
+    const optionOne = e.target.value
+
+    this.setState(() => ({
+      optionOne
+    }))
+  }
+
+  handleOptionTwoChange = (e) => {
+    const optionTwo = e.target.value
+
+    this.setState(() => ({
+      optionTwo
+    }))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log("NewQuestion current state:")
+    console.log(this.state)
+  }
+
   render() {
+    const { optionOne, optionTwo } = this.state
+
     return (
-      <div>The new question form goes here!</div>
+      <div>
+        <h1>Create New Question</h1>
+        <p>Option 1:</p>
+        <form>
+          <input
+            value={optionOne}
+            onChange={this.handleOptionOneChange}
+            type="text" />
+          <p>Option 2:</p>
+          <input
+            value={optionTwo}
+            onChange={this.handleOptionTwoChange}
+            type="text" />
+          <div>
+            <button onClick={this.handleSubmit}>
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
 
-function mapStateToProps () {
-  return {}
-}
-
-export default connect(mapStateToProps)(NewQuestion)
+export default connect()(NewQuestion)
