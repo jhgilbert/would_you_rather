83b879f7012e2f6b5dad65b2c79a5cab61dccd3a
Jennifer Gilbert <j.h.gilbert@gmail.com>
2019-07-04 09:54:45 -0700

    feat: Connect questions reducer to the new functionality

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,31 +1,16 @@
-import { ADD_QUESTION, ADD_VOTE_TO_QUESTION } from '../actions/questions'
+import { ADD_QUESTION, ADD_VOTE_TO_QUESTION, RECEIVE_QUESTIONS } from '../actions/questions'
 
 function generateUID () {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
 
 export default function questions (state = {}, action) {
-  if (Object.keys(state).length === 0) {
-    console.log("Populating initial state of fake questions ...")
-    state = {
-      'testQuestion1': {
-        id: 'testQuestion1',
-        optionOne: { text: 'run a race', votes: [] },
-        optionTwo: { text: 'climb a tree', votes: [] },
-        author: 'billy_bob',
-        timestamp: Date.now(),
-      },
-      'testQuestion2': {
-        id: 'testQuestion2',
-        optionOne: { text: 'go to a party', votes: [] },
-        optionTwo: { text: 'stay home all night', votes: [] },
-        author: 'jane_doe',
-        timestamp: Date.now(),
-      }
-    }
-  }
-
   switch(action.type) {
+    case RECEIVE_QUESTIONS :
+    return {
+      ...state,
+      ...action.questions
+    }
     case ADD_QUESTION :
       let newQuestion = action.question
       newQuestion.id = generateUID()
