a886b2a934d9896fc9b7a37c27c61ebfa0e7a591
Jennifer Gilbert <j.h.gilbert@gmail.com>
2019-07-04 09:29:51 -0700

    feat: Add action to receive questions from fake API

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,5 +1,13 @@
 export const ADD_QUESTION = 'ADD_QUESTION'
 export const ADD_VOTE_TO_QUESTION = 'ADD_VOTE_TO_QUESTION'
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+
+export function receiveQuestions (questions) {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions,
+  }
+}
 
 export function addQuestion (question, authedUser) {
   return {
