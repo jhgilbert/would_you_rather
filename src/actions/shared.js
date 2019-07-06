66e72d52ff08ad21e3ad7647b5098b26125dce21
Jennifer Gilbert <j.h.gilbert@gmail.com>
2019-07-06 13:23:23 -0700

    feat: Add test mode

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,9 +2,10 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveQuestions } from '../actions/questions'
 import { initializeLoading, completeLoading } from '../actions/loading'
-// import { setAuthedUser } from '../actions/authedUser'
+import { setAuthedUser } from '../actions/authedUser'
 
-// const AUTHED_USER = "sarahedo"
+const AUTHED_USER = "sarahedo"
+const TEST_MODE = true
 
 export function handleInitialData () {
   return (dispatch) => {
@@ -14,7 +15,9 @@ export function handleInitialData () {
         dispatch(receiveUsers(users))
         dispatch(receiveQuestions(questions))
         dispatch(completeLoading())
-        // dispatch(setAuthedUser(AUTHED_USER))
+        if (TEST_MODE) {
+          dispatch(setAuthedUser(AUTHED_USER))
+        }
       })
   }
 }
